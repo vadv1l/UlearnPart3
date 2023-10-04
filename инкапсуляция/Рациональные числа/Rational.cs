8ce25f27e0e5ce4afb4c9ca2720419cb06a1fa0d
vadv1l <116947675+vadv1l@users.noreply.github.com>
2023-10-04 20:24:51 +0500

    Инкапсуляция

---
+++ инкапсуляция/Рациональные числа/Rational.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation.RationalNumbers
+{
+     public class Rational
+    {
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+        public bool IsNan => Denominator == 0; 
+        public Rational(int numerator,int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+            ZeroNumerator();
+            Minus();
+            ReduceRational();
+        }
+        public Rational(int numerator)
+        {
+            Numerator = numerator;
+            Denominator = 1;
+        }
+        public void ZeroNumerator()
+        {
+            if (Numerator == 0)
+            {
+                Denominator = 1;
+            }
+        }
+        public void Minus()
+        {
+            if (Denominator < 0)
+            {
+                Denominator = -Denominator;
+                Numerator = -Numerator;
+            }
+        }
+        public void ReduceRational()
+        {
+            for (int i = Math.Abs(Numerator); i>0; i--)
+            {
+                if (Numerator % i == 0 && Denominator % i == 0)
+                {
+                    Numerator /= i;
+                    Denominator /= i;
+                }
+            }
+        }
+        public static Rational operator +(Rational r1, Rational r2)
+        {
+            if (r1.IsNan || r2.IsNan)
+            {
+                return new Rational(1, 0);
+            }
+            return new Rational(r1.Numerator * r2.Denominator + r2.Numerator * r1.Denominator, r1.Denominator * r2.Denominator);
+        }
+        public static Rational operator -(Rational r1, Rational r2)
+        {
+            if (r1.IsNan || r2.IsNan)
+            {
+                return new Rational(1, 0);
+            }
+            return new Rational(r1.Numerator * r2.Denominator - r2.Numerator * r1.Denominator, r1.Denominator * r2.Denominator);
+        }
+        public static Rational operator *(Rational r1, Rational r2)
+        {
+            if (r1.IsNan || r2.IsNan)
+            {
+                return new Rational(1, 0);
+            }
+            return new Rational(r1.Numerator*r2.Numerator,r1.Denominator*r2.Denominator);
+        }
+        public static Rational operator /(Rational r1, Rational r2)
+        {
+            if (r1.IsNan || r2.IsNan)
+            {
+                return new Rational(1, 0);
+            }
+            return new Rational(r1.Numerator * r2.Denominator, r1.Denominator * r2.Numerator);
+        }
+        public static implicit operator double(Rational r1)
+        {
+            if (r1.IsNan||r1.Numerator==0)
+            {
+                return double.NaN;
+            }
+            return r1.Numerator/(double)r1.Denominator;
+        }
+        public static explicit operator int(Rational r1)
+        {
+            if (r1.Numerator % r1.Denominator != 0)
+            {
+                throw new Exception();
+            }
+            else
+            {
+                int numer = r1.Numerator / r1.Denominator;
+                return numer;
+            }
+        }
+        public static implicit operator Rational(int number)
+        {
+            return new Rational(number);
+        }
+    }
+}
---
+++ инкапсуляция/Сбои/ReportMaker.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation.Failures
+{
+
+    public class Common
+    {
+        /*public static int IsFailureSerious(int failureType)
+        {
+            if (failureType%2==0) return 1;
+            return 0;
+        }*/
+
+
+        public static int Earlier(object[] v, int day, int month, int year)
+        {
+            int vYear = (int)v[2];
+            int vMonth = (int)v[1];
+            int vDay = (int)v[0];
+            if (vYear < year) return 1;
+            if (vYear > year) return 0;
+            if (vMonth < month) return 1;
+            if (vMonth > month) return 0;
+            if (vDay < day) return 1;
+            return 0;
+        }
+    }
+
+    public class ReportMaker
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="day"></param>
+        /// <param name="failureTypes">
+        /// 0 for unexpected shutdown, 
+        /// 1 for short non-responding, 
+        /// 2 for hardware failures, 
+        /// 3 for connection problems
+        /// </param>
+        /// <param name="deviceId"></param>
+        /// <param name="times"></param>
+        /// <param name="devices"></param>
+        /// <returns></returns>
+        public static List<string> FindDevicesFailedBeforeDateObsolete(
+            int day,
+            int month,
+            int year,
+            int[] failureTypes, 
+            int[] deviceId, 
+            object[][] times,
+            List<Dictionary<string, object>> devices)
+        {
+
+            var problematicDevices = new HashSet<int>();
+            for (int i = 0; i < failureTypes.Length; i++)
+                if (Common.IsFailureSerious(failureTypes[i]) && Common.Earlier(times[i], day, month, year)==1)
+                    problematicDevices.Add(deviceId[i]);
+
+            var result = new List<string>();
+            foreach (var device in devices)
+                if (problematicDevices.Contains((int)device["DeviceId"]))
+                    result.Add(device["Name"] as string);
+
+            return result;
+        }
+        
+        public class Device
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public override bool Equals(object obj)
+            {
+                if (obj is Device device)
+                {
+                    return device.Id == Id && device.Name == Name;
+                }
+                else return false;
+            }
+        }
+        enum FailureType
+        {
+            UnexpectedShutDown = 0,
+            ShortNonResponding = 1,
+            HardwareFailures = 2,
+            ConnectionProblems = 3
+        }
+        public class Failure
+        {
+            public FailureType FailureType { get; set; }
+            public bool IsFailureSerious(){return (int) FailureType%2==0; }
+
+        }
+        public static List<string> FindDevicesFailedBeforeData(DateTime data, Failure[]failures,)
+        {
+            
+        }
+           
+    }
+}
